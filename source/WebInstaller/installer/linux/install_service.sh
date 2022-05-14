0472549e63d566dd4f49099d0ab5350371e91343
bastiansbits <sebastian.loesch@icloud.com>
2022-05-14 22:11:46 +0200

    Added downlaod for new files

--- source/WebInstaller/installer/linux/install_service.sh
+++ source/WebInstaller/installer/linux/install_service.sh
@@ -2,11 +2,44 @@
 
 INSTALL_DIR=/Users/$(whoami)/Documents/webinstaller_test
 INSTALL_URL=http://localhost:8080/installer/linux
+FILE_DIR=http://localhost:8080/files/zip
 
+## Move to target directory
+if [ ! -d ${INSTALL_DIR} ]; then 
+  mkdir -p ${INSTALL_DIR}
+  if [ $? != 0 ]; then
+    echo "Failed to create installation directory"
+    exit 1
+  fi
+fi 
+pushd ${INSTALL_DIR}
+
+## Create or Update the installation file
 if [[ "$1" != "no-update" ]]; then
   # cUrl has extremely limited possibility. To get the filename from the server, we first have to download it
   INSTALL_NAME=$(curl -O -J -L -v ${INSTALL_URL} 2>&1 | grep "Content-Disposition" | grep -Eo 'filename=[^;]+' -)
-  if [ "${INSTALL_NAME}" == "" ]; then echo "Could not download installer file"; exit 1; fi
+  if [ "${INSTALL_NAME}" == "" ]; then echo "Failed to reach the sever"; exit 1; fi
   if [ -f ./${INSTALL_NAME} ]; then rm ./${INSTALL_NAME}; fi
   INSTALL_NAME=$(curl -O -J -L -v ${INSTALL_URL} 2>&1 | grep "Content-Disposition" | grep -Eo 'filename=[^;]+' -)
+  if [ -f ./${INSTALL_NAME} ]; then echo "Failed to download the installation file"; exit 1; fi
 fi
+
+## Clean up /tmp
+if [ -f /tmp/changes.zip ]; then rm /tmp/changes.zip; fi
+if [ -d /tmp/changes ]; then rm -rf /tmp/changes; fi
+  
+if [ -f ./manifest.json ]; then
+  ## Send the manifest and get changes
+  curl -X GET http://localhost:8080/files/zip \
+    -H 'Content-Type: application/json' \
+    -d $(cat ./manifest.json) \
+    -o /tmp/changes.zip
+else
+  ## Send an empty body and get everything
+  curl -X GET http://localhost:8080/files/zip \
+    -H 'Content-Type: application/json' \
+    -d '{ "files": [] }' \
+    -o /tmp/changes.zip
+fi
+
+unzip /tmp/changes.zip -d /tmp/changes
