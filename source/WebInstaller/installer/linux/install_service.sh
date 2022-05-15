cf5974a24df45265dc302add9544a76e989df414
bastiansbits <sebastian.loesch@icloud.com>
2022-05-15 10:28:13 +0200

    negate condition

--- source/WebInstaller/installer/linux/install_service.sh
+++ source/WebInstaller/installer/linux/install_service.sh
@@ -25,7 +25,7 @@ if [[ "$1" != "no-update" ]]; then
   if [ "${INSTALL_NAME}" == "" ]; then echo "Failed to reach the sever"; exit 1; fi
   if [ -f ./${INSTALL_NAME} ]; then rm ./${INSTALL_NAME}; fi
   INSTALL_NAME=$(curl -O -J -L -v ${INSTALL_URL} 2>&1 | grep "Content-Disposition" | grep -Eo 'filename=[^;]+' - | tr "=" "\n" | sed -n 2p)
-  if [ -f ./${INSTALL_NAME} ]; then echo "Failed to download the installation file"; exit 1; fi
+  if [ ! -f ./${INSTALL_NAME} ]; then echo "Failed to download the installation file"; exit 1; fi
   /bin/bash ./${INSTALL_NAME} "no-update"
   exit 1
 fi
